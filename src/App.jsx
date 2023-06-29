ba6740aa4dddb9c90dd51244280ebff11f0dfb5b
Luis <lpaz0073@gmail.com>
2023-06-29 16:19:16 -0500

    fix: build images

--- src/App.jsx
+++ src/App.jsx
@@ -11,28 +11,28 @@ export const App = () => {
 			title: "Disfruta en tu TV",
 			description:
 				"Ve en smart TV, PlayStation, Xbox, Chromecast, Apple TV, reproductores de Blu-ray y más.",
-			img: "./src/assets/img/tv.png",
+			img: "tv.png",
 		},
 		{
 			id: 2,
 			title: "Descarga tus series para verlas offline",
 			description:
 				"Guarda tus títulos favoritos y siempre tendrás algo que ver.",
-			img: "./src/assets/img/mobile-0819.jpg",
+			img: "mobile-0819.jpg",
 		},
 		{
 			id: 3,
 			title: "Disfruta donde quieras",
 			description:
 				"Películas y series ilimitadas en tu teléfono, tablet, computadora y TV sin costo extra.",
-			img: "./src/assets/img/device-pile.png",
+			img: "device-pile.png",
 		},
 		{
 			id: 4,
 			title: "Crea perfiles para niños",
 			description:
 				"Los niños vivirán aventuras con sus personajes favoritos en un espacio diseñado exclusivamente para ellos, sin costo con tu membresía.",
-			img: "./src/assets/img/ninios.png",
+			img: "ninios.png",
 		},
 	]);
 
--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -12,7 +12,7 @@ export const FrontPage = () => {
 	};
 
 	return (
-		<main className="bg-[url('./src/assets/img/portada.jpg')] bg-cover bg-no-repeat bg-center relative h-[55vh] md:h-[70vh]">
+		<main className="bg-[url('../portada.jpg')] bg-cover bg-no-repeat bg-center relative h-[55vh] md:h-[70vh]">
 			<div className='absolute inset-0 z-30' style={borderHero}></div>
 			<div className='absolute inset-0 z-20' style={bgHero}></div>
 			<div className='relative container mx-auto px-8 md:px-16 xl:px-28 py-5 flex flex-col h-full z-50'>
